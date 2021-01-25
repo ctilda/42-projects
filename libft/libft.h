76fbf15f459e6726f180f036d2d2141b5d7340cc
ctilda <61395218+ctilda@users.noreply.github.com>
2021-01-26 00:23:55 +0300

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -36,7 +36,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putstr(char const *s);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putendl(char const *s);
 void	ft_putnbr_fd(int n, int fd);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t destsize);
@@ -50,5 +52,14 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+void    ft_lstiter(t_list *lst, void (*f)(void *));
+void ft_lstclear(t_list **lst, void (*del)(void *));
+void ft_lstdelone(t_list *lst, void (*del)(void *));
+void ft_lstadd_back(t_list **lst, t_list *new);
+t_list *ft_lstlast(t_list *lst);
+int ft_lstsize(t_list *lst);
+void ft_lstadd_front(t_list **lst, t_list *new);
+t_list *ft_lstnew(void *content);
 
 #endif
