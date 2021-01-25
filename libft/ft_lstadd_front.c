949a66e829e4e1e26201464326e3052a53b252b1
ctilda <61395218+ctilda@users.noreply.github.com>
2021-01-26 00:24:43 +0300

    Update ft_lstadd_front.c

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -1,10 +1,10 @@
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **alst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (alst && new)
+	if (lst && new)
 	{
-		new->next = *alst;
-		*alst = new;
+		new->next = *lst;
+		*lst = new;
 	}
 }
